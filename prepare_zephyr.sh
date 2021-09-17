82e3e578afc59323237f57ebe79077e0a1bcc963
MarWit <contact@marwit.rs>
2021-09-17 13:14:23 +0200

    Declutter wget output

--- prepare_zephyr.sh
+++ prepare_zephyr.sh
@@ -6,7 +6,7 @@ ZEPHYR_SOURCE_URL="https://github.com/zephyrproject-rtos/zephyr/archive/${ZEPHYR
 mkdir zephyr
 pushd zephyr
 
-wget -Ozephyr.zip "$ZEPHYR_SOURCE_URL"
+wget --progress=dot:giga -Ozephyr.zip "$ZEPHYR_SOURCE_URL"
 unzip zephyr.zip
 rm zephyr.zip
 mv zephyr-* zephyr
@@ -39,7 +39,7 @@ ZEPHYR_SDK_URL="$ZEPHYR_SDK_RELEASES_URL/v$SDK_VERSION/$ZEPHYR_SDK_FILENAME"
 export ZEPHYR_TOOLCHAIN_VARIANT=zephyr
 export ZEPHYR_SDK_INSTALL_DIR=$(pwd)/zephyr/sdk
 
-wget $ZEPHYR_SDK_URL
+wget --progress=dot:giga $ZEPHYR_SDK_URL
 chmod +x $ZEPHYR_SDK_FILENAME
 ./$ZEPHYR_SDK_FILENAME -- -y -d $ZEPHYR_SDK_INSTALL_DIR
 rm $ZEPHYR_SDK_FILENAME
