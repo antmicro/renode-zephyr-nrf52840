cea19a924e5a37e17a1b95dc3abe409f6c863a6f
MarWit <contact@marwit.rs>
2021-09-17 11:14:27 +0200

    Add configuration for GitHub Actions

--- build_binaries.sh
+++ build_binaries.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -xeu
 
 export ZEPHYR_TOOLCHAIN_VARIANT=zephyr
+export ZEPHYR_BASE=$(pwd)/zephyr
 export ZEPHYR_SDK_INSTALL_DIR=$(pwd)/zephyr-sdk
 
 mkdir artifacts
 pushd zephyr
 
-west build -p auto -b nrf52840dk_nrf52840 samples/hello_world
+west build -p auto -b nrf52840dk_nrf52840 samples/hello_world -- -G'Unix Makefiles'
 cp build/zephyr/zephyr.elf ../artifacts/zephyr-hello_world.elf
 
-west build -p auto -b nrf52840dk_nrf52840 samples/subsys/shell/shell_module
+west build -p auto -b nrf52840dk_nrf52840 samples/subsys/shell/shell_module -- -G'Unix Makefiles'
 cp build/zephyr/zephyr.elf ../artifacts/zephyr-shell_module.elf
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,3 +4,6 @@ set -e
 
 ./prepare_zephyr.sh
 ./build_binaries.sh
+
+test.sh nrf52840.robot
+cp -r report.html log.html robot_output.html artifacts
